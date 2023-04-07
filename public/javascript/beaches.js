3fa8d748d52563437d9b557ecdcef868760a058f
Mike Kaipis <kaipismike1@gmail.com>
2023-04-07 12:26:38 +0300

    map

--- public/javascript/beaches.js
+++ public/javascript/beaches.js
@@ -16,3 +16,14 @@ document.querySelector('.page-description').addEventListener("click",(event)=>{
         e.classList.add("hidden-map");
     }
 })
+
+
+var map = L.map('map').setView([37.9448, 23.3539], 10);
+
+// gia nea views toy map
+//https://leaflet-extras.github.io/leaflet-providers/preview/#filter=Jawg.Terrain
+
+var osm = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+});
+osm.addTo(map);
